778d6d96a1454f5d7e56cd9c6de79fa970e4099f
kio <kio@little-bat.de>
2025-02-23 10:54:17 +0100

    version

--- Source/version.h
+++ Source/version.h
@@ -6,8 +6,8 @@
 
 #define APPL_VERSION_H	  0
 #define APPL_VERSION_M	  8
-#define APPL_VERSION_L	  33
-#define APPL_VERSION_STR  "0.8.33"
+#define APPL_VERSION_L	  34
+#define APPL_VERSION_STR  "0.8.34"
 #define APPL_VERSION_BETA true
 
 static constexpr char startup_info_message[] =
