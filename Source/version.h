dd9f00934bdf18d9006dd5095c385c365d72392f
kio <kio@little-bat.de>
2025-10-23 00:16:24 +0200

    version.h

--- Source/version.h
+++ Source/version.h
@@ -6,16 +6,16 @@
 
 #define APPL_VERSION_H	  0
 #define APPL_VERSION_M	  8
-#define APPL_VERSION_L	  43
-#define APPL_VERSION_STR  "0.8.43"
+#define APPL_VERSION_L	  44
+#define APPL_VERSION_STR  "0.8.44"
 #define APPL_VERSION_BETA true
 
-static constexpr char startup_info_message[] =
-	"Welcome to version " APPL_VERSION_STR
-	" of zxsp.\n"
-	"This version fixes some regressions related to debugging: "
-	"hanging after stepIn and stepOut and the missing video beam indicator.\n"
-	"Please report any issue at\n"
-	"github.com/Megatokio/zxsp/issues.";
+static constexpr char startup_info_message[] = "Welcome to version " APPL_VERSION_STR
+											   " of zxsp.\n"
+											   "This version fixes the disappeared TC2068 cartridge dock, "
+											   "adds register edit for the Didaktik Melodik, "
+											   "adds preference for joystick overlays and fixes some errors.\n"
+											   "Please report any issue at\n"
+											   "github.com/Megatokio/zxsp/issues.";
 
 static constexpr char check_update_url[] = "https://k1.spdns.de/cgi-bin/zxsp-check-update.cgi?version=";
